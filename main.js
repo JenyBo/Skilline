4c8fc99757142dc9e58e047e037afdfc338d100b
unknown <you@example.com>
2022-08-21 19:33:10 +0700

    my first

---
+++ main.js
@@ -0,0 +1,19 @@
+// var courses = ['HTML & CSS', 'Javascript', 'PHP', 'Java']
+
+// function render(courses) {
+//     document.querySelector('ul').innerHTML = courses.map(x =>`<h1>${x}<h1\>`).join('');
+// }
+
+// var boxElement = document.querySelector('.box');
+// console.log(boxElement);
+
+// console.log(render(courses));
+var x = '1';
+var y = '2';
+console.log(x + '+ ' + y);
+console.log(parseFloat(x+y));
+// Object.assign(boxElement.style,{
+//     width : '100px',
+//     height : '200px',   
+//     backgroundColor : 'red',
+// });
